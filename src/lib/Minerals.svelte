3ea676eb33e49d63034a8de688c94247dbce0fa9
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-02-11 06:50:54 +0200

    Quick and dirty fetch to ensure that CORS works

---
+++ src/lib/Minerals.svelte
@@ -0,0 +1,40 @@
+<script>
+    import { onMount } from 'svelte'
+
+    let data = []
+
+    async function getMinerals () {
+        const data = []
+
+        const response = await fetch("http://ninhursag.localdomain:5000/api/v1/minerals")
+        if (!response.ok) throw Error('Bad response!')
+
+        const minerals = await response.json()
+
+        for (const mineral in minerals) {
+            const url = minerals[mineral]
+
+            data.push({name: mineral, url})
+        }
+
+        return data
+    }
+
+    onMount(() => {
+        data = getMinerals()
+    })
+</script>
+
+<code>
+    {#await data}
+        Loading...
+    {:then minerals}
+        <ul>
+        {#each minerals as mineral}
+            <li>{ mineral.name }</li>
+        {/each}
+        </ul>
+    {:catch error}
+        {error}
+    {/await}
+</code>

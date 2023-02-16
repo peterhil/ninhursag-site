523f522a0535c4d2bfb6da03eaa000e1083cbac3
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-02-16 09:12:07 +0200

    Add existing frontend code

---
+++ src/lib/component/Notifications.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Alert from './Alert.svelte'
+	import { alerts } from '../stores/alerts'
+</script>
+
+<div id="notifications">
+	{#each [...$alerts.entries()] as [id, alert] (id) }
+		<Alert type={alert.type} on:click={() => alerts.remove(id)}>
+			{alert.message}
+		</Alert>
+	{/each}
+</div>

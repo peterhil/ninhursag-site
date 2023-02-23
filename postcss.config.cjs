001df06d2b412aaeb6e2155f5bc0b900a536008b
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-02-23 05:41:41 +0200

    postcss: Add mixins to plugins

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,10 @@
 const autoprefixer = require('autoprefixer');
+const mixins = require('postcss-mixins');
 const nested = require('postcss-nested');
 
 const config = {
 	plugins: [
+		mixins,
 		nested,
 		autoprefixer,
 	],

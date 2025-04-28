156cb63985fd7eba71f75dc423f8f4b9084def17
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-04-28 22:11:27 +0300

    deps: eslint: Update eslint plugins and config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,59 +1,50 @@
-import path from "node:path"
-import { fileURLToPath } from "node:url"
+import globals from 'globals'
+import js from '@eslint/js'
+import prettier from 'eslint-config-prettier'
+import svelte from 'eslint-plugin-svelte'
+import svelteConfig from './svelte.config.js'
+import ts from 'typescript-eslint'
+import { fileURLToPath } from 'node:url'
+import { includeIgnoreFile } from '@eslint/compat'
 
-import globals from "globals"
-import js from "@eslint/js"
-import svelte3 from "eslint-plugin-svelte3"
-import tsParser from "@typescript-eslint/parser"
-import typescriptEslint from "@typescript-eslint/eslint-plugin"
-import { FlatCompat } from "@eslint/eslintrc"
-import { defineConfig, globalIgnores } from "eslint/config"
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url))
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-})
-
-export default defineConfig([
-	globalIgnores([
-		"!**/.env.example",
-		"**/*.cjs",
-		"**/.DS_Store",
-		"**/.env",
-		"**/.env.*",
-		"**/node_modules",
-		"**/package-lock.json",
-		"**/pnpm-lock.yaml",
-		"**/yarn.lock",
-		".svelte-kit",
-		"build",
-		"package",
-	]),
+export default ts.config(
+	includeIgnoreFile(gitignorePath),
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs.recommended,
+	prettier,
+	...svelte.configs.prettier,
 	{
-		extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"),
-		plugins: {
-			svelte3,
-			"@typescript-eslint": typescriptEslint,
-		},
 		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node,
-			},
-			parser: tsParser,
-			ecmaVersion: 2020,
-			sourceType: "module",
+			globals: { ...globals.browser, ...globals.node }
 		},
-		settings: {
-			"svelte3/typescript": "typescript",
+		rules: {
+			'no-undef': 'off'
 		},
 	},
 	{
-		files: ["**/*.svelte"],
-		processor: "svelte3/svelte3",
-	}
-])
+		files: [
+			'**/*.svelte',
+			'**/*.svelte.ts',
+			'**/*.ts',
+			'**/*.svelte.js',
+		],
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				extraFileExtensions: ['.svelte'],
+				parser: ts.parser,
+				svelteConfig
+			}
+		},
+		rules: {
+			'brace-style': ['error', 'stroustrup', {
+				allowSingleLine: true,
+			}],
+			'comma-dangle': ['off', 'always'],
+			'no-console': ['off', 'always'],
+		},
+	},
+)

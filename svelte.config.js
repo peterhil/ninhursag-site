a84346456b1a16c78f8ea68b638a44d017b41832
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-04-28 22:42:05 +0300

    svelte: ts: Fix configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,20 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-auto'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [
 		vitePreprocess(),
 		preprocess({
 			scss: {
-				prependData: '@use "src/variables.scss" as *;'
+				prependData: '@use "src/variables.scss" as *'
 			},
-
 			postcss: true
 		})
 	],
-
 	kit: {
 		adapter: adapter(),
 		alias: {
@@ -25,6 +23,6 @@ const config = {
 			$style: 'src/style',
 		},
 	}
-};
+}
 
-export default config;
+export default config
